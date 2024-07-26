Added correct scaling

demov4.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from cv2 import FONT_HERSHEY_SIMPLEX, LINE_AA, WINDOW_KEEPRATIO, putText, namedWindow, imshow, resizeWindow, waitKey, destroyAllWindows
-from numpy import uint8, inf, min, nan, int32, isnan, ndarray, linalg, bool_, argwhere, vstack, argmin, argwhere, mean, sum, full, copy, zeros, array
+from cv2 import FONT_HERSHEY_SIMPLEX, LINE_AA, WINDOW_KEEPRATIO, COLOR_BGR2GRAY, putText, namedWindow, imshow, resizeWindow, waitKey, destroyAllWindows, cvtColor
+import cv2
+from numpy import uint8, inf, min, nan, int32, isnan, quantile, ndarray, linalg, bool_, array, argwhere, vstack, argmin, argwhere, mean, sum, full, copy, zeros, array
 from time import time, sleep
 from copy import deepcopy
 import pyny3d.geoms as pyny
@@ -21,17 +22,32 @@ Based on https://github.com/geaxgx/depthai_hand_tracker and on the works of Ultr
 
 path_to_yolo = "/home/ema/Desktop/depthai-hand-segmentation/models/best_nano8.pt"
 camera_fps = 15
-num_elements_moving_avarage = 15
+num_elements_moving_avarage = 18
 hand_obj_dist_threshold = 60 #[mm]
-down_sampling = 5
-black_pixel_threshold = 30
+down_sampling = 3
 
-# obj_class_area_threshold = np.array([139.5, 428.5, 386.5])
-obj_class_area_threshold = np.array([139.5, 428.5, 276])
+# obj_class_area_threshold = array([139.5, 428.5, 386.5]) oggetti interi calcolati con metro
+# obj_class_area_threshold = array([139.5, 428.5, 276])  oggetti interi, con camera
+# obj_class_area_threshold = array([45, 256, 37]) # 40 / 50, 230 / 300, 28 / 46, camera 51 / 60, 
+obj_class_area_threshold = array([[10, 45, 100],[20, 428.5, 1000], [20, 95, 180]]) # 40 / 50, 230 / 300, 28 / 46, camera 51 / 60, 
+
+# define range of black color in HSV
+hsv_v_lower_val = 0
+hsv_v_upper_val = 60
+black_pixel_threshold = 75
+kernel_side = 9
+kernel_half_side = int(kernel_side/2)
+kernel_elements_num = kernel_side * kernel_side
 
 DISPLAY = True
 SCALING = True
 EXO_COMM = False
+
+def switch_x_y_column(arr):
+    new_arr = np.zeros(arr.shape, dtype=np.uint8)
+    new_arr[:,0] = arr[:,1]
+    new_arr[:,1] = arr[:,0]
+    return new_arr
 
 def add_grid_img(gesture_status, object_name, object_weight, fps, grid_height, grid_width, cell_height, cell_width):
     '''
@@ -40,16 +56,16 @@ def add_grid_img(gesture_status, object_name, object_weight, fps, grid_height, g
     grid_img = zeros((grid_height, grid_width, 3), dtype=uint8)
 
     # Adds fixed text labels
-    putText(grid_img, f'Status:', (10, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
-    putText(grid_img, f'Object:', (2*cell_width-30, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
-    putText(grid_img, f'Weight:', (10, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
-    putText(grid_img, f'Fps:', (2*cell_width - 30, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, f'Status:', (10, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, f'Object:', (2*cell_width-50, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, f'Weight:', (10, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, f'Fps:', (2*cell_width - 50, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
 
     # Adds dynamic data. Modify the blank spaces and font dimension to obtain a better visualization. 
-    putText(grid_img, ' ' + gesture_status, (90, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
-    putText(grid_img, object_name, (2*cell_width + 60, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
-    putText(grid_img, ' ' + object_weight +' kg', (90, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
-    putText(grid_img, f'{fps}', (2*cell_width + 60, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, ' ' + gesture_status, (90, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, object_name, (2*cell_width + 30, cell_height // 2 + 5), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, ' ' + object_weight +' kg', (90, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
+    putText(grid_img, f'{fps}', (2*cell_width + 30, cell_height + cell_height // 2), FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, LINE_AA)
 
     return grid_img
 
@@ -144,43 +160,117 @@ def calc_distances(masks_pixels, depth_frame, hand_spatial):
 def calc_area(selected_contour_pixels, selected_contour_spatials, grasped_object, rgb_frame, black_pixel_threshold):
     dark_contour_pixel_indices = []
     area = None
+    area_selected_contour_pixels = np.copy(selected_contour_pixels)
+    area_selected_contour_spatials = np.copy(selected_contour_spatials)
 
-    # if grasped_object in ["Hammer", "Crimper"]:
-    #     if selected_contour_pixels is not None:
-    #         selected_contour_pixels = array(selected_contour_pixels)
-    #         # Extract the RGB values at the specified coordinates, assuming selected_contour_pixels is in (x, y) format
-    #         pixel_values = rgb_frame[selected_contour_pixels[:, 0], selected_contour_pixels[:, 1]]
-    #         # Find indices where pixel intensity is below the black_pixel_threshold (for any RGB channel)
-    #         dark_contour_pixel_indices = argwhere((pixel_values < black_pixel_threshold).any(axis=-1))
-    #         selected_contour_spatials = selected_contour_spatials[dark_contour_pixel_indices]
+    if grasped_object in ["Hammer", "Crimper"] and selected_contour_pixels is not None:
+        # gray_frame = cvtColor(rgb_frame, COLOR_BGR2GRAY)
+        selected_contour_pixels = array(selected_contour_pixels)
+        '''# Extract the RGB values at the specified coordinates, assuming selected_contour_pixels is in (y, x) format to match OpenCV conventions
+        pixel_values = rgb_frame[selected_contour_pixels[:, 0], selected_contour_pixels[:, 1]]
+        # Find indices where pixel intensity is below the black_pixel_threshold (for any RGB channel)
+        dark_contour_pixel_indices = argwhere((pixel_values < black_pixel_threshold).all(axis=-1)).flatten()
+        
+        if selected_contour_spatials is not None:
+            selected_contour_spatials = array(selected_contour_spatials)
+            selected_contour_spatials = selected_contour_spatials[dark_contour_pixel_indices]
+            # selected_contour_spatials = selected_contour_spatials[dark_contour_pixel_indices]'''
+         # Convert BGR to HSV
+        hsv_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2HSV)
 
-    if selected_contour_spatials is not None and len(selected_contour_spatials) > 0:
+        
+
+        pixels_hsv_values = hsv_frame[selected_contour_pixels[:, 0], selected_contour_pixels[:, 1]]
+        dark_pixels_indeces = np.argwhere(pixels_hsv_values[:,2] < hsv_v_upper_val)
+        dark_pixels = selected_contour_pixels[dark_pixels_indeces,:].reshape(-1,2)
+        dark_spatials = selected_contour_spatials[dark_pixels_indeces,:].reshape(-1,3)
+        pixels_hsv_values = pixels_hsv_values[dark_pixels_indeces,:].reshape(-1,3)
+        if dark_pixels.shape[0] < 1:
+            print("no black values: ", dark_pixels)
+            return None, None, None
+        
+        dark_pixels_indeces = np.argwhere(pixels_hsv_values[:,2] > hsv_v_lower_val)
+        dark_pixels = dark_pixels[dark_pixels_indeces,:].reshape(-1,2)
+        dark_spatials = dark_spatials[dark_pixels_indeces,:].reshape(-1,3)
+        pixels_hsv_values = pixels_hsv_values[dark_pixels_indeces,:].reshape(-1,3)
+        if dark_pixels.shape[0] < 1:
+            print("no black values: ", dark_pixels)
+            return None, None, None
+
+
+        binary_frame = np.zeros((rgb_frame.shape), dtype=np.bool_)
+        #dark_pixels_yx = switch_x_y_column(dark_pixels) # yx -> xy
+        binary_frame[dark_pixels[:,0], dark_pixels[:,1]] = binary_frame[dark_pixels[:,0], dark_pixels[:,1]] + 1
+
+        final_indices = []
+        for i in range(dark_pixels.shape[0]):
+            val = np.sum(binary_frame[dark_pixels[i,0]-kernel_half_side:dark_pixels[i,0]+kernel_half_side+1,
+                                      dark_pixels[i,1]-kernel_half_side:dark_pixels[i,1]+kernel_half_side+1])
+            # print("val: ", val)
+            if val > kernel_elements_num/3:
+                final_indices.append(i)
+
+
+        area_selected_contour_pixels = np.copy(dark_pixels[final_indices,:])
+        area_selected_contour_spatials = np.copy(dark_spatials[final_indices,:])
+
+        # area_selected_contour_pixels = np.copy(dark_pixels)
+        # area_selected_contour_spatials = np.copy(dark_spatials)
+        
+        
+        # selected_spatials = selected_spatials[dark_pixel_indices].reshape((-1,3))
+        # # print("selected_spatials shape: ", selected_spatials.shape)
+        # selected_pixels = selected_pixels[dark_pixel_indices].reshape((-1,2))
+        # # print("selected_pixels shape: ", selected_pixels.shape)
+
+        # binary_frame = np.zeros((frame.shape), dtype=np.bool_)
+        # print("selected_pixels\n", selected_pixels)
+        # selected_pixels = switch_x_y_column(selected_pixels) # yx -> xy
+        # binary_frame[selected_pixels] = binary_frame[selected_pixels] + 1
+
+        # for pixel in selected_pixels:
+        #     # y, x = pixel
+        #     # gray_frame[y, x] = np.sum(gray_frame[y-kernel_side:y+kernel_side+1, x-kernel_side:x+kernel_side+1] * mean_kernel)
+        #     print("mean_kernel: ", mean_kernel)
+        #     binary_frame[pixel] = np.bool_(np.sum(gray_frame[pixel[0]-kernel_half_side:pixel[0]+kernel_half_side+1,
+        #                                                         pixel[1]-kernel_half_side:pixel[1]+kernel_half_side+1] * mean_kernel))
+        #     # gray_frame[pixel[:,0],pixel[:,1]] = np.sum[gray_frame[pixel[:,0] - (kernel_side/2):pixel[:,0] + (kernel_side/2),
+        #     #                                           pixel[:,1] - (kernel_side/2):pixel[:,1] + (kernel_side/2)]]*kernel
+
+        # pluto_indices = []
+        # for i in range(selected_pixels.shape[0]):
+        #     if binary_frame[selected_pixels[i]] == 1:
+        #         pluto_indices.append(i)
+
+        # selected_spatials = selected_spatials[pluto_indices]
+        # selected_pixels = selected_pixels[pluto_indices]
+
+    
+
+    if area_selected_contour_spatials is not None and len(area_selected_contour_spatials) > 0:
         try:
-            # Calculate the object's distance using the depth frame
-            obj_z = np.quantile(selected_contour_spatials[:,2], 0.8)
+            #Calculate the object's distance using the depth frame
+            obj_z = quantile(selected_contour_spatials[:,2], 0.9)
             if obj_z < 300 or obj_z > 1100:
-                print("ops")
-                return None
-            
-            for i in range(selected_contour_spatials.shape[0]):
-                selected_contour_spatials[i,2] = obj_z
+                print("Objects out of range for area calculation!")
+                return None, None, None
+
+            #Set all z-values to obj_z
+            selected_contour_spatials[:, 2] = obj_z
 
             # Flatten the list of arrays to ensure homogeneity
-            selected_contour_spatials_flat = [item for sublist in selected_contour_spatials for item in sublist]
+            # selected_contour_spatials_flat = [item for sublist in selected_contour_spatials for item in sublist]
             # Convert to numpy array, ensuring the correct shape
-            selected_contour_spatials_np = array(selected_contour_spatials_flat).reshape(-1, 3)
-
-            polygon = pyny.Polygon(selected_contour_spatials_np)
+            # selected_contour_spatials_np = array(selected_contour_spatials_flat).reshape(-1, 3)
+            # polygon = pyny.Polygon(selected_contour_spatials_np)
+            polygon = pyny.Polygon(area_selected_contour_spatials)
             area = (polygon.get_area())/100
-            print(f'Area is: {int(area)} cm^2')
-            return area
+            #print(f'Area is: {int(area)} cm^2')
 
         except Exception as e:
             print(f"Error while processing selected_contour_spatials: {e}")
 
-    return area
-
-
+    return area, area_selected_contour_pixels, area_selected_contour_spatials
 
 if __name__ == '__main__':
     '''
@@ -322,8 +412,7 @@ if __name__ == '__main__':
             if handsData_i_2 and hand_spatial is not None:
                 if grasping_status:
                     if len(mask_contour_indices_i_2) > 0:
-########################################################################################################################################################################
-                        selected_index, selected_obj_spatial, selected_dist, selected_contour_spatials, selected_contour_pixels = calc_distances(mask_contour_indices_i_2, depth_frame_i_2, hand_spatial)
+                        selected_index, selected_obj_spatial, selected_dist, selected_contour_spatials, selected_contour_pixels = calc_distances(masks_indices_i_2, depth_frame_i_2, hand_spatial)
                         if selected_index is not None and selected_index >= 0 and selected_index < len(inference_class_list_i_2):
                             grasped_obj_name = my_seg8.get_class_names()[inference_class_list_i_2[selected_index]]
                             obj_class_id = inference_class_list_i_2[selected_index]
@@ -355,18 +444,19 @@ if __name__ == '__main__':
 
                             if hand_obj_dist < hand_obj_dist_threshold:
                                 grasping_status_arr[moving_avarage_index] = True
-                                print("ciao")
-                                if SCALING:
-                                    area = calc_area(selected_contour_pixels, selected_contour_spatials, grasped_obj_name, frame_i_2, black_pixel_threshold)
+                                if SCALING and released:
+                                    area, area_selected_contour_pixels, area_selected_contour_spatials = calc_area(selected_contour_pixels, selected_contour_spatials, grasped_obj_name, frame_i_2, black_pixel_threshold)
                                     area_arr[moving_avarage_index] = area
-                                    print("area: ", area)
+                                    #selected_contour_pixels_yx = switch_x_y_column(selected_contour_pixels)
+                                    ####print("selected_contour_pixels_yx[black_pixel_indices]\n", selected_contour_pixels_yx[black_pixel_indices])
+                                    if area_selected_contour_pixels is not None:
+                                        annotated_frame_i_2[area_selected_contour_pixels[:,0], area_selected_contour_pixels[:,1]] = annotated_frame_i_2[area_selected_contour_pixels[:,0], area_selected_contour_pixels[:,1]] * [255,255,255]
                             else:
                                 grasping_status_arr[moving_avarage_index] = False
 
                             # Update moving average index
                             moving_avarage_index = (moving_avarage_index + 1) % num_elements_moving_avarage
 
-##########################################################################################################################################################
                 else:
                     # Handle case when the hand is not grasping
                     grasping_status_arr[moving_avarage_index] = False
@@ -382,16 +472,18 @@ if __name__ == '__main__':
             if sum(grasping_status_arr) > num_elements_moving_avarage/2:
                 if released:
                     last_grasped_obj_name = grasped_obj_name
-                    print("obj_class_id: ", obj_class_id)
+                    # print("obj_class_id: ", obj_class_id)
                     obj_weight = my_seg8.get_class_weight()[obj_class_id][0]
                     if SCALING:
                         # compute weight
                         print("area_arr: ", area_arr)
-                        good_values_area_arr_indeces = np.argwhere(area_arr > 0)
+                        good_values_area_arr_indeces = argwhere(area_arr > obj_class_area_threshold[obj_class_id,0])
                         area_values = area_arr[good_values_area_arr_indeces]
-                        mean_area = np.mean(area_values)
-                        print("\n\n\n\n\narea mean: ", mean_area)
-                        if mean_area < obj_class_area_threshold[obj_class_id]:
+                        good_values_area_arr_indeces = argwhere(area_arr < obj_class_area_threshold[obj_class_id,2])
+                        area_values = area_arr[good_values_area_arr_indeces]
+                        mean_area = quantile(area_values, 0.9)
+                        print("\n\n\narea mean: ", mean_area)
+                        if mean_area < obj_class_area_threshold[obj_class_id,1]:
                             obj_weight = my_seg8.get_class_weight()[obj_class_id][0]
                             last_grasped_obj_name = last_grasped_obj_name + " small"
                         else:
@@ -415,7 +507,6 @@ if __name__ == '__main__':
                 last_obj_weight = "-"
                 grasping_status_avarage = False 
 
-                
             if DISPLAY:
                 # Draw hand annotations on the frame
                 frame_final = handRender.draw(annotated_frame_i_2, handsData_i_2)
@@ -426,12 +517,11 @@ if __name__ == '__main__':
                 # Stack the main frame and the grid image vertically
                 combined_img = vstack((frame_final, grid_img))
 
-                namedWindow('Object segmentation & grasping detection', WINDOW_KEEPRATIO)
+                # Show the final frame
                 imshow('Object segmentation & grasping detection', combined_img)
-                resizeWindow('Object segmentation & grasping detection', 1280, 720)
 
                 # Break the loop if 'q' key is pressed
-                if waitKey(10) == ord('q'):
+                if waitKey(1) == ord('q'):
                     break
             else:
                 print("fps: ", fps)