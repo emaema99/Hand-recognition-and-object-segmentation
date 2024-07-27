d55d5e37aca405df49ba71314529ef61c6c4f89f
emaema99 <dodge5399@gmail.com>
2024-07-27 13:15:42 +0200

    Start terminal when Jetson connected

---
+++ scaling_vision_launcher.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/jetson/Desktop/hand_track_obj_seg8_final/
+gnome-terminal --window-with-profile=keepOpen -- "./scaling_vision_startup.sh" 
---
+++ scaling_vision_startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#source ~/virtual_env/user_localisator/bin/activate
+
+python3 /home/jetson/Desktop/hand_track_obj_seg8_final/demov4.py
+
