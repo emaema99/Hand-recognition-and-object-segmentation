29b95473a8648063fd0de6d76c16c4cf8ab7f8a4
emaema99 <dodge5399@gmail.com>
2024-11-04 10:44:55 +0100

    Updated file path

--- scaling_vision_startup.sh
+++ scaling_vision_startup.sh
@@ -2,5 +2,5 @@
 
 #source ~/virtual_env/user_localisator/bin/activate
 
-python3 /home/jetson/Desktop/hand_track_obj_seg8_final/demov4.py
+python3 /home/jetson/Desktop/hand_track_obj_seg8_final/demov8.py
 
